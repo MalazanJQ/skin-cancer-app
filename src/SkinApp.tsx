1ecbb8c3b4442ea86fb2c732aa032f006489c8c7
MalazanThrone <114039823+MalazanThrone@users.noreply.github.com>
2024-06-14 15:54:16 -0700

    fixing dependencies

--- src/SkinApp.tsx
+++ src/SkinApp.tsx
@@ -5,64 +5,67 @@ import axios from 'axios';
 
 /** Displays the UI of the Home Page. */
 export default function SkinApp() {
-  const [preview, setPreview] = useState();
+  const [preview, setPreview] = useState<string | undefined>(undefined);
   const [predict, setPredict] = useState<React.ReactNode>(undefined);
-  const [button, setButton] = useState(<div></div>);
-  const [image, setImage] = useState();
+  const [button, setButton] = useState<React.ReactNode>(<div></div>);
+  const [image, setImage] = useState<File | undefined>(undefined);
 
-  function previewImage(e) {
-    if (e.target.files[0]) {
+  function previewImage(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e.target.files && e.target.files[0]) {
       const image = URL.createObjectURL(e.target.files[0]);
       setPreview(image);
       setImage(e.target.files[0]);
     }
   }
 
-  const predictImage = async () => {
+  const predictImage = useCallback(async () => {
     setButton(<div></div>);
     setPredict(<p className="mt-4">Loading model prediction...</p>);
 
-    const formData = new FormData();
-    formData.append('file', image);
+    if (image) {
+      const formData = new FormData();
+      formData.append('file', image);
 
-    try {
-      const response = await axios.post('http://localhost:5000/upload', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-      setPredict(
-        <div>
-          <p className="text-center mt-4">Predicted class: {response.data.prediction}</p>
-          <p>Benign probability: {response.data.benign}</p>
-          <p>Maglinant probability: {response.data.malignant}</p>
-        </div>
-      );
-    } catch (error) {
-      console.error("Error in predicting the image!", error);
+      try {
+        const response = await axios.post('http://localhost:5000/upload', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+        setPredict(
+          <div>
+            <p className="text-center mt-4">Predicted class: {response.data.prediction}</p>
+            <p>Benign probability: {response.data.benign}</p>
+            <p>Malignant probability: {response.data.malignant}</p>
+          </div>
+        );
+      } catch (error) {
+        console.error("Error in predicting the image!", error);
+      }
     }
-  }
+  }, [image]);
 
   useEffect(() => {
     if (preview) {
       setButton(<button className="btn btn-secondary mt-4" onClick={predictImage}>Predict</button>);
-      setPredict(null)
+      setPredict(null);
     } else {
       setButton(<div></div>);
     }
   }, [preview, predictImage]);
 
+  /** Renders the correct page */
   return (
     <div className="container mt-4">
       <h2 className="mb-4">Machine Learning Model to Classify Skin Cancer</h2>
-      <p class="text-danger">
-        WARNING: Not to be used to diagnose any disease. <br></br>This model was trained solely as an exercise to apply machine learning concepts
+      <p className="text-danger">
+        WARNING: Not to be used to diagnose any disease. <br />This model was trained solely as an exercise to apply machine learning concepts
         and its predictions should not be used in any capacity.
       </p>
       <h4 className="mb-4">Upload an Image</h4>
-      <div class="input-group mb-3">
-        <input type="file" accept="image/*" class="form-control" id="inputGroupFile02" onChange={previewImage}/>
-        <label class="input-group-text" for="inputGroupFile02">Upload</label>
+      <div className="input-group mb-3">
+        <input type="file" accept="image/*" className="form-control" id="inputGroupFile02" onChange={previewImage} />
+        <label className="input-group-text" htmlFor="inputGroupFile02">Upload</label>
       </div>
       {preview && (
         <div className="container text-center mt-4">
@@ -78,7 +81,8 @@ export default function SkinApp() {
           </div>
         </div>
       )}
-      <span>This model was trained on the following dataset:<br></br>
+      <span>This model was trained on the following dataset:<br />
       Muhammad Hasnain Javid. (2022). Melanoma Skin Cancer Dataset of 10000 Images [Data set]. Kaggle. https://doi.org/10.34740/KAGGLE/DSV/3376422</span>
-    </div>);
+    </div>
+  );
 };
